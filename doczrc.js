045f0d19c399cf88880b3d78e3bf6b27d4425486
walker smith <wsmith@collab.net>
2018-11-07 10:37:36 -0500

    intiial commit

---
+++ doczrc.js
@@ -0,0 +1,7 @@
+export default {
+  htmlContext: {
+    head: {
+      raw: [`<svg><symbol id="myDot" width="10" height="10" viewBox="0 0 2 2"><circle cx="1" cy="1" r="1" /></symbol></svg>`]
+    }
+  }
+};
